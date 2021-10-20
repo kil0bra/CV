dec3a3b6b9cd3d0b19614ed9df42f54786796e07
kil0bra <92576044+kil0bra@users.noreply.github.com>
2021-10-20 12:56:42 +0300

    Create CV

---
+++ app.js
@@ -0,0 +1,66 @@
+// переменные
+//camelCase
+//const firstName = 'Kirill'
+//const age = 23               // number
+//const isProgrammer = true // boolean
+
+//const _ = 'private'
+//const $ = 'some value'
+
+//const if = 'mkef' // error
+//const withNum5 = '5'
+//const 5withNum = 5 // error
+
+// 2 мутирование
+//console.log('Имя человека: ' + firstName + ', а возраст: ' + age)
+//const lastName = prompt ('введите фамилию')
+//alert(firstName +  ' ' + lastName)
+
+// 3 операторы 
+//let currentYear = 2021
+//const birthYear = 1998
+//const age = currentYear - birthYear
+//const a = 10
+//const b = 5
+//let c = 32
+//c = c+a
+ // 4 типы данных
+
+//  const isProgrammer = true
+//  const firstName = 'Kirill'
+//  const age = 23
+//  let x
+//  console.log (typeof isProgrammer)
+//  console.log (typeof firstName)
+//  console.log (typeof age)
+//  console.log (typeof x)
+// console.log (null)
+
+// 5. Приоритет операторов
+//const fullAge = 23
+//const birthYear = 1998
+//const currentYear = 2021
+
+//const isFullAge = currentYear - birthYear >= fullAge // 26>=27
+//console.log(isFullAge)
+
+//6. Условные операторы
+//const courseStatus = 'fail' // ready, fail, pending
+
+//if (courseStatus === 'ready') {
+  //console.log('Курс уже готов и его можно проходить')
+//}
+
+ //else if (courseStatus === 'pending') {
+  //console.log('Курс пока находится в процеесе разработки')
+//}
+//else {
+//  console.log('Курс не получился')
+//}
+//const isReady = true
+//if (isReady) {
+ // console.log('Все готово!')
+//} else {
+//console.log('Все не готово!')
+//}
+//isReady ? console.log('Все готово'): console.log('Все не готово')-Тернарное выражение
